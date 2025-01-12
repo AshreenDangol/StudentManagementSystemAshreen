64b0dc2876c51d6931c02a2793a6df27fe908d85
AshreenDangol <np03cs4a230223@heraldcollege.edu.np>
2025-01-12 13:59:41 +0545

    adding main file and student file int the git repo

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student{
+
+    public String name;
+    public int age;
+    void Student(){
+        System.out.println("the name of the student is" + name + ", and the age of the student is  " + age);
+
+    }
+}
+
+public static void main(String[]args){
+    Student s =new Student()
+    s.Student()
+
+    
+}
