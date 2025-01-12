e3089d5c97e9ab0631792be6f784a4023e9b6317
AshreenDangol <np03cs4a230223@heraldcollege.edu.np>
2025-01-12 14:14:25 +0545

    added inside parts in student file

--- Student.java
+++ Student.java
@@ -2,15 +2,18 @@ public class Student{
 
     public String name;
     public int age;
-    void Student(){
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;
+
         System.out.println("the name of the student is" + name + ", and the age of the student is  " + age);
 
     }
-}
 
-public static void main(String[]args){
-    Student s =new Student()
-    s.Student()
+public static void main(String[]args)
+    {
+        Student s = new Student("Ashreen", 21);
 
-    
+        
+    };
 }
